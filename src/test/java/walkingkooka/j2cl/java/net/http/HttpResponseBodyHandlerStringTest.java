2997ecbc710fd26d44acaf825721aa8237f1d945
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2020-08-02 18:53:58 +1000

    HttpResponse.BodyHandlers

---
+++ src/test/java/walkingkooka/j2cl/java/net/http/HttpResponseBodyHandlerStringTest.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2020 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.j2cl.java.net.http;
+
+import org.junit.jupiter.api.Test;
+import walkingkooka.j2cl.java.net.http.HttpClient.Version;
+import walkingkooka.j2cl.java.net.http.HttpResponse.ResponseInfo;
+
+import java.net.http.HttpHeaders;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public final class HttpResponseBodyHandlerStringTest extends HttpResponseBodyHandlerTestCase2<HttpResponseBodyHandlerString, String> {
+
+    @Test
+    public void testWithNullCharsetFails() {
+        assertThrows(NullPointerException.class, () -> HttpResponseBodyHandlerString.with(null));
+    }
+
+    @Test
+    public final void testApplyResponseInfo() {
+        final HttpResponseBodyHandlerString handler = HttpResponseBodyHandlerString.with(UTF8);
+        assertEquals(HttpResponseBodySubscriberString.class,
+                handler.apply(new ResponseInfo() {
+                    @Override
+                    public HttpHeaders headers() {
+                        throw new UnsupportedOperationException();
+                    }
+
+                    @Override
+                    public int statusCode() {
+                        throw new UnsupportedOperationException();
+                    }
+
+                    @Override
+                    public Version version() {
+                        throw new UnsupportedOperationException();
+                    }
+                }).getClass());
+    }
+
+    @Test
+    public void testToString() {
+        this.toStringAndCheck(HttpResponseBodyHandlerString.with(UTF8), "HttpResponse.ofString " + UTF8);
+    }
+
+    @Override
+    HttpResponseBodyHandlerString createBodyHandler() {
+        return HttpResponseBodyHandlerString.with(UTF8);
+    }
+
+    @Override
+    public Class<HttpResponseBodyHandlerString> type() {
+        return HttpResponseBodyHandlerString.class;
+    }
+}
